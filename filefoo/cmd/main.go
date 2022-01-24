b253a78ce259e0c6562b2976e8a665fd1d0cafe0
sohailnajar <contact.sohail@gmail.com>
2022-01-24 16:48:19 +0100

    Ex6.2 - named file

--- filefoo/cmd/main.go
+++ filefoo/cmd/main.go
@@ -7,9 +7,11 @@ import (
 
 func main() {
 	var size int
+	var name string
 	flag.IntVar(&size, "size", 0, "size of the zeros")
+	flag.StringVar(&name, "name", "zeroes.dat", "name of the file")
 	flag.Parse()
 
-	filefoo.BufferdWrite("zeroes.dat", size)
+	filefoo.BufferdWrite(name, size)
 
 }
