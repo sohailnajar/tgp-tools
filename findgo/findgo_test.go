d519355942f8b163adbbcab1b68e850ba5a89026
sohailnajar <contact.sohail@gmail.com>
2022-01-28 08:42:33 +0100

    benchmarking

--- findgo/findgo_test.go
+++ findgo/findgo_test.go
@@ -2,6 +2,7 @@ package findgo_test
 
 import (
 	"findgo"
+	"os"
 	"testing"
 	"testing/fstest"
 )
@@ -18,4 +19,36 @@ func TestFiles(t *testing.T) {
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
+}
+
+func TestFilesOnDisk(t *testing.T) {
+	t.Parallel()
+	fsys := os.DirFS("testdata")
+	want := 4
+	got := findgo.Files(fsys)
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func BenchmarkFilesOnDisk(b *testing.B) {
+	fsys := os.DirFS("testdata")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		findgo.Files(fsys)
+	}
+}
+
+func BenchmarkFilesInMemory(b *testing.B) {
+	fsys := fstest.MapFS{
+		"file.go":                {},
+		"subfolder/subfolder.go": {},
+		"subfolder2/another.go":  {},
+		"subfolder2/file.go":     {},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		findgo.Files(fsys)
+	}
+
 }
